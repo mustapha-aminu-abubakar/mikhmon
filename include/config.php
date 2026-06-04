211e74be6831da9e93673f03e00f921001375043
Mustapha Aminu Abubakar <amustee11@gmail.com>
2026-06-04 18:47:36 +0100

    chore: stop tracking config.php, add sample template

--- include/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
-
-$data['new-7371'] = array ('1'=>'new-7371!','new-7371@|@','new-7371#|#','new-7371%','new-7371^','new-7371&Rp','new-7371*10','new-7371(1','new-7371)','new-7371=10','new-7371@!@disable');
-$data['new-8395'] = array ('1'=>'new-8395!','new-8395@|@','new-8395#|#','new-8395%','new-8395^','new-8395&Rp','new-8395*10','new-8395(1','new-8395)','new-8395=10','new-8395@!@disable');
